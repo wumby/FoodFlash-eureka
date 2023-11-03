2ac981e9547b684e3e7e1c564b8f7255a91686b8
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2023-11-03 12:25:58 -0600

    init

---
+++ eureka/src/test/java/com/jack/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
