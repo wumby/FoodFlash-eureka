aa1f600f27f6c8a2a354e844709693faf440b904
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2023-12-04 11:01:27 -0700

    k8s

--- src/main/java/com/codedecode/eureka/EurekaApplication.java
+++ src/main/java/com/codedecode/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.jack.eureka;
+package com.codedecode.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/codedecode/eureka/EurekaApplicationTests.java
+++ src/test/java/com/codedecode/eureka/EurekaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jack.eureka;
+package com.codedecode.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
